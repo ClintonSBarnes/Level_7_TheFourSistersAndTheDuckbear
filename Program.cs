883d3a508bb38e8622a9ed3c1a905851004e238b
ClintonSBarnes <clintbarnes8@gmail.com>
2022-05-22 12:27:42 -0500

    This is pracitce with %'s

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int totalEggs;
+int eggsPerSister;
+int eggsForDuckbear;
+
+Console.WriteLine(
+    "Four sisters own a chocolate farm and collect chocolate eggs laid by " +
+    "chocolate chickens everyday.\nAll four sisters get an equal number of " +
+    "chocolate eggs every day, \nand the remainder is fed to their pet duckbear.\n" +
+    "How many chocolate eggs were laid by the chocolate chickens? ");
+
+totalEggs = Convert.ToInt16(Console.ReadLine());
+eggsPerSister = totalEggs / 4;
+eggsForDuckbear = totalEggs % 4;
+
+Console.WriteLine("\nEach sister will receive " + eggsPerSister + " ,and duckbear will receive " + eggsForDuckbear + " eggs.");
+
+Console.ReadKey();
